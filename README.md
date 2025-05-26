4f17c18bc95135ec14be61ecae19c946bd3b534a
komalanami <kumarikomal1511@gmail.com>
2025-05-27 00:31:16 +0530

    Adding initial Comment

---
+++ README.md
@@ -0,0 +1,4 @@
+### When creating a project with only the Spring Security and Spring Web dependencies
+
+By default, Spring Security enables basic form login and HTTP Basic authentication.
+A default user is created, and a generated password is displayed in the console when the application starts.
---
+++ src/main/java/com/ideastack/learn_spring_security/Controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.ideastack.learn_spring_security.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+}
